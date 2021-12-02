a41b31273c9e115800e1b26f2fb82ee5391312c4
Jason Cummings <jasoncummings86@gmail.com>
2021-12-01 22:06:50 -0500

    add default params in ruby

--- day1/main.rb
+++ day1/main.rb
@@ -1,6 +1,8 @@
-def sonarSweep(input, priorSum, increased)
+def sonarSweep(input, priorSum = 0, increased = 0)
   return increased if input.length < 3
   currentSum = input[0..2].reduce(:+)
   increased += 1 if currentSum > priorSum && priorSum != 0
   sonarSweep(input[1..], currentSum, increased)
 end
+
+puts sonarSweep(input)
