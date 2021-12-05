308759cd2613085acd8298879fe4551efbf41a3f
Jason Cummings <jasoncummings86@gmail.com>
2021-12-05 14:44:39 -0500

    day 5 part 2 ruby

--- day5/main.rb
+++ day5/main.rb
@@ -1,16 +1,21 @@
-def parse_input
+def find_vents
   field = Hash.new(0)
+
   File.open("input.txt").each_line do |line|
-    x1, y1, x2, y2 = line.scan /\d+/
+    x1, y1, x2, y2 = line.scan(/\d+/).map(&:to_i)
+    is_diagonal = ((x1 - x2).abs == (y1 - y2).abs)
 
-    next unless (x1 == x2 || y1 == y2)
+    next unless (x1 == x2 || y1 == y2 || is_diagonal)
 
-    x1 = x1.to_i
-    x2 = x2.to_i
-    y1 = y1.to_i
-    y2 = y2.to_i
+    if is_diagonal
+      field["#{x2},#{y2}"] += 1
 
-    if x1 == x2
+      while "#{x1},#{y1}" != "#{x2},#{y2}" do
+        field["#{x1},#{y1}"] += 1
+        x1 < x2 ? x1 += 1 : x1 -= 1
+        y1 < y2 ? y1 += 1 : y1 -= 1
+      end
+    elsif x1 == x2
       ([y1, y2].min..[y1, y2].max).each do |y|
         field["#{x1},#{y}"] += 1
       end
@@ -24,5 +29,5 @@ def parse_input
   puts field.values.count {|x| x > 1}
 end
 
-parse_input
+find_vents
 
