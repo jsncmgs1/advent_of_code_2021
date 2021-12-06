5f2a0d2335b47cadcb6a22663b32ba92abbd2d01
Jason Cummings <jasoncummings86@gmail.com>
2021-12-06 16:25:23 -0500

    refactored into using an array instead of hash

--- day6/main.rb
+++ day6/main.rb
@@ -1,42 +1,23 @@
 require 'benchmark'
 
 def simulate_lanternfish(days)
-  input = File.read("input.txt").gsub(/\n/, "").split(",")
+  input = File.read("input.txt").gsub(/\n/, "").split(",").map(&:to_i)
 
   fish_count = 0
-  spawn_table = {
-    "0" => 0,
-    "1" => 0,
-    "2" => 0,
-    "3" => 0,
-    "4" => 0,
-    "5" => 0,
-    "6" => 0,
-    "7" => 0,
-    "8" => 0
-  }
-
+  spawn_table = Array.new(9, 0)
   input.each do |n|
     fish_count += 1
     spawn_table[n] += 1
   end
 
-  puts Benchmark.measure {
-    days.times do
-      new_or_recently_duplicated = spawn_table["0"]
-      fish_count += new_or_recently_duplicated
+  days.times do
+    new_or_recently_duplicated = spawn_table[0]
+    fish_count += new_or_recently_duplicated
 
-      spawn_table["0"] = spawn_table["1"]
-      spawn_table["1"] = spawn_table["2"]
-      spawn_table["2"] = spawn_table["3"]
-      spawn_table["3"] = spawn_table["4"]
-      spawn_table["4"] = spawn_table["5"]
-      spawn_table["5"] = spawn_table["6"]
-      spawn_table["6"] = spawn_table["7"] + new_or_recently_duplicated
-      spawn_table["7"] = spawn_table["8"]
-      spawn_table["8"] = new_or_recently_duplicated
-    end
-  }
+    spawn_table.shift
+    spawn_table[6] += new_or_recently_duplicated
+    spawn_table[8] = new_or_recently_duplicated
+  end
 
   puts fish_count
 end
