8b77b06a88d0e0cb7c3d492bfa13e636594e7b5c
Jason Cummings <jasoncummings86@gmail.com>
2021-12-07 09:52:19 -0500

    day7 part 1 ruby

--- day7/main.rb
+++ day7/main.rb
@@ -1,19 +1,19 @@
 def crab_walk
-  input = File.read('input.txt').gsub(/\n/, "").split(",").map(&:to_i)
-  require 'pry'; binding.pry
+  input = File.read('input.txt')
+    .gsub(/\n/, "")
+    .split(",")
+    .map(&:to_i)
+    .sort!
 
+  # yes I know this won't work for every median set
+  # but it works for mine and I live to AOC another day
+  median,_ = input[input.length/2.0]
 
-end
-
-def medians(sorted_array)
-  return nil if array.empty?
-  len = array.length
-
-  if len.odd?
-    return array[(array/2.0).ceil]
-  else
-    return
+  steps = input.reduce(0) do |acc, n|
+    acc += (n - median).abs
   end
+
+  puts steps
 end
 
 crab_walk
