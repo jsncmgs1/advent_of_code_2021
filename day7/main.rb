8ef0003e823efaa7e8e58c88979253a8658985e1
Jason Cummings <jasoncummings86@gmail.com>
2021-12-07 10:20:38 -0500

    day 7 part 2 ruby

--- day7/main.rb
+++ day7/main.rb
@@ -1,19 +1,22 @@
 def crab_walk
-  input = File.read('input.txt')
-    .gsub(/\n/, "")
-    .split(",")
-    .map(&:to_i)
-    .sort!
+  input = File.read('input.txt').gsub(/\n/, "").split(",").map(&:to_i)
 
-  # yes I know this won't work for every median set
-  # but it works for mine and I live to AOC another day
-  median,_ = input[input.length/2.0]
+  avg = (input.sum / input.length.to_f)
+  ceil = avg.ceil
+  floor = avg.floor
 
-  steps = input.reduce(0) do |acc, n|
-    acc += (n - median).abs
-  end
+  calculate_steps = ->(avg){
+    input.reduce(0) do |acc, n|
+      num_steps = (n - avg).abs
+      step_gas_usage = (num_steps*(num_steps+1))/2 # sum of all nums 1..n =  (n*(n+1))/2
+      acc += step_gas_usage
+    end
+  }
 
-  puts steps
+  with_ceil = calculate_steps.call(ceil)
+  with_floor = calculate_steps.call(floor)
+
+  puts [with_ceil, with_floor].min
 end
 
 crab_walk
